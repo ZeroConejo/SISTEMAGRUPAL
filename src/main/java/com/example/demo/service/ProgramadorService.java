60f9accdfe617b9b33e3f7236468c6f5cde655cf
DiegoAlexander23senati <diegodianderas23@gmail.com>
2025-05-15 10:40:06 -0500

    se actualizo PorgtramadorService

--- src/main/java/com/example/demo/service/ProgramadorService.java
+++ src/main/java/com/example/demo/service/ProgramadorService.java
@@ -1,5 +1,37 @@
 package com.example.demo.service;
 
-public class ProgramadorService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProgramadorService {
+	@Autowired
+	private ProgramadorRepository repository;
+	
+	public List<Programador> getAll(){
+		return repository.findAll();
+	}
+	
+	public Optional<Pogramador> getById(integer id){
+		return repository.findById(id);
+	}
+	
+	public Programador create(Programador programador) {
+		return repository.save(programador);
+	}
+	
+	public Programador update(integer id,Programador programador) {
+		if (repository.existsById(id)) {
+			programador.setIdprogramador(id);
+			return repository.save(programador);
+		}
+		return null;
+	}
+	
+	public void delete(integer id) {
+		repository.deleteById(id);
+	}
 }
